57705ea9961ee2e2c8a027f8edad1fd52708b87d
Dan Herrera <danh@shew.io>
2024-09-20 17:37:33 -0400

    chore(scripts/new): address shellcheck lint

--- scripts/new.sh
+++ scripts/new.sh
@@ -9,8 +9,8 @@ set -ex
 
 NAME=$1
 SLUG=$2
-SED_ESCAPED_NAME=$(echo "${NAME}" | sed -e 's/&/\&/g')
+SED_ESCAPED_NAME=${NAME//&/\&}
 TARGET="content/posts/${SLUG}.md"
 
-hugo new content $TARGET
+hugo new content "${TARGET}"
 sed -i "s/REPLACEME/${SED_ESCAPED_NAME}/" "${TARGET}"
